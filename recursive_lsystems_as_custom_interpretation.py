269acbe7206ff9f57f67c0483270c3d0e5b42902
Krister Ljung <krister.ljung@speedmail.se>
2020-02-22 21:53:10 +0100

    Start of a flower

--- recursive_lsystems_as_custom_interpretation.py
+++ recursive_lsystems_as_custom_interpretation.py
@@ -24,9 +24,10 @@ def leaf(turtle, parameters, bl_obj, obj_base_pairs, context):
         ob.select_set(False)
     lex = lsystem.exec.Exec()
     lex.set_axiom("p(line)X")
-    lex.add_rule("X", "FX")
+    lex.add_rule("X", "FX") #todo: make it more leaf shaped
     lex.min_iterations = int(float(parameters[0]))
     lex.exec(context=context)
+    # Go through all the objects in the child lsystem and set position, rotation, and parent
     for obj in lex.objects:
         obj.location = lsystem.util.matmul(turtle.transform, mathutils.Vector((0.0, 0.0, 0.0)))
         obj.rotation_euler = turtle.transform.to_euler()
@@ -38,7 +39,12 @@ def flower(turtle, parameters, bl_obj, obj_base_pairs, context):
     for ob in bpy.context.selected_objects:
         ob.select_set(False)
     lex = lsystem.exec.Exec()  # Problem with original lsystem being selected
-    # todo: add rules and such
+    # todo: make the flower unfold
+    # todo: expand the size of the leafs
+    lex.define("a0", "45")
+    lex.define("a1", "12.5")
+    lex.set_axiom("p(line)LLLLLLLL")
+    lex.add_rule("L", "/(a0)f(0)[^(60)¤(0.4)F^(-a1)¤(0.75)F^(-a1)¤(0.5)F^(-a1)¤(0.35)F^(-a1)¤(0.05)F]")
     lex.min_iterations = int(float(parameters[0]))
     lex.exec(context=context)
     for obj in lex.objects:
@@ -54,9 +60,9 @@ ex.set_axiom("p(subsurf)a(1)")
 ex.add_rule("a(t)", "F(1)[&(30)L(0)]/(137.5)a(add(t,1))", "lt(t,7)")
 ex.add_rule("a(t)", "F(20)A", "eq(t,7)")
 ex.add_rule("A", "K(0)")
-ex.add_rule("L(t)", "L(add(t,1))", "lt(t,9)")  # todo: create Leaf
-ex.add_rule("K(t)", "K(add(t,1))", "lt(t,5)")  # todo: create flower
+ex.add_rule("L(t)", "L(add(t,1))", "lt(t,9)")  # create Leaf
+ex.add_rule("K(t)", "K(add(t,1))", "lt(t,5)")  # create flower
 ex.add_rule("F(l)", "F(add(l,0.2))", "lt(l,2)")
 ex.set_interpretation("L", leaf)
 ex.set_interpretation("K", flower)
-ex.exec(min_iterations=7)
+ex.exec(min_iterations=11)
