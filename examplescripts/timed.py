40299f7abcb1695d5a6f4e6122f54724c3ea1ad5
Krister Ljung <krister.ljung@speedmail.se>
2021-01-02 18:17:22 +0100

    Move example scripts to sub directory

--- examplescripts/timed.py
+++ examplescripts/timed.py
@@ -1,4 +1,11 @@
 # Make an l-system with a "time" parameter and a "detail" parameter
 # For each "time" step the lsystem has to do a predetermined number of "detail" iterations
 
+import lsystem.exec
+import mathutils
+
+
+ex = lsystem.exec.Exec()
 # todo
+ex.exec(min_iterations=11)
+
